519c33e1b4f72d84e02a1e432a241cddf17729b8
samuelmensah <samuelnmensah@live.com>
2014-05-28 22:22:44 -0700

    added custom exception class

---
+++ CustomExceptions/CustomExceptions/OddNumberException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomExceptions
+{
+    /// <summary>
+    /// Represents an odd number exception that will occur during the excecution of the program
+    /// </summary>
+    [Serializable]
+    public class OddNumberException : Exception
+    {
+        public int OddNumber { get; set; }
+        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("OddNumber",OddNumber);
+        }
+        public OddNumberException(){}
+        public OddNumberException(int oddNumber) 
+        {
+            this.OddNumber = oddNumber;
+        }
+
+        public OddNumberException(string message) : base(message){}
+    }
+}
+/*
+Steps to create a custom exception
+ * Give it a name
+ * Add at least one constructor 
+ 
+
+
+*/
--- CustomExceptions/CustomExceptions/Program.cs
+++ CustomExceptions/CustomExceptions/Program.cs
@@ -10,6 +10,20 @@ namespace CustomExceptions
     {
         static void Main(string[] args)
         {
+            var input = new List<int> {2,4,5,8,10};
+            try
+            {
+                foreach (var i in input)
+                {
+                    if(i%2 !=0)
+                        throw new OddNumberException(i);
+                }
+            }
+            catch (OddNumberException exception)
+            {
+                Console.Write(string.Format("An odd Number was found in the sequence {0}",exception.OddNumber));
+                Console.ReadKey();
+            }
         }
     }
 }
